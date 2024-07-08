40d9f5ad27229e6b2942791cd750c8f648f27df4
Giridharan Mohan <giridharan.mohan@rapiddatatech.com>
2024-07-08 21:08:48 +0400

    fix: InputListener

--- main.go
+++ main.go
@@ -21,6 +21,8 @@ var pages = tview.NewPages()
 
 var TodoList = tview.NewList().ShowSecondaryText(false)
 
+var TodoTextView = tview.NewTextView()
+
 var flex = tview.NewFlex()
 
 func addTodoForm() {
@@ -41,19 +43,29 @@ func addTodoForm() {
 }
 
 func addTodoList() {
+	TodoList.Clear()
 	for index, todo := range todo_list {
 		TodoList.AddItem(todo.task, "", rune(49+index), nil)
 	}
 }
 
+func setTodoText(todo *Todo) {
+	TodoTextView.Clear()
+	text := todo.task + "\n" + todo.endDate
+	TodoTextView.SetText(text)
+}
+
 func main() {
 	flex.SetDirection(tview.FlexRow).
-		AddItem(TodoList, 0, 1, true).
+		AddItem(tview.NewFlex().
+			AddItem(tview.NewTextView().SetTextColor(tcell.ColorGreen).SetText("To Do List"), 0, 1, false).
+			AddItem(TodoList, 0, 1, true).
+			AddItem(TodoTextView, 0, 2, false), 0, 4, false).
 		AddItem(text, 0, 1, false)
 
-	pages.AddPage("Menu", flex, true, true)
-	pages.AddPage("Add Form", form, true, false)
-	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+	pages.AddPage("Menu", flex, true, true).SetTitle("Menu")
+	pages.AddPage("Add Form", form, true, false).SetTitle("TodoForm")
+	flex.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		if event.Rune() == 113 {
 			app.Stop()
 		} else if event.Rune() == 97 {
